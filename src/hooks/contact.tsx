19463d267f547fd86d41a15d5b91acf199efdd44
fellypsantos <fellypsantos2011@gmail.com>
2023-08-21 11:32:09 -0300

    refactored addContact hook

--- src/hooks/contact.tsx
+++ src/hooks/contact.tsx
@@ -1,17 +1,10 @@
-import React, {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import React, { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import { Alert, Linking, ToastAndroid } from 'react-native';
 import AlertAsync from 'react-native-alert-async';
 
 import IContact from '../interfaces/IContact';
-import { useDatabase } from './database';
 import { useAppTranslation } from './translation';
+import DataBase from '../databases';
 
 interface ContactContext {
   contacts: IContact[];
@@ -32,32 +25,27 @@ const ContactContext = createContext<ContactContext | null>(null);
 const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [contacts, setContacts] = useState<IContact[]>([]);
-  const { dbConnection } = useDatabase();
   const { Translate } = useAppTranslation();
 
   const addContact = useCallback(
-    async (contact: IContact) => {
+    (contact: IContact) => {
       try {
         const { id, name, country_code, phone, country, createdAt } = contact;
 
-        const insertResult = await dbConnection?.executeSql(
-          'INSERT INTO contacts(id, name, country_code, phone, country, createdAt) VALUES(?,?,?,?,?,?)',
-          [id, name, country_code, phone, country, createdAt],
-        );
-
-        if (insertResult) {
-          setContacts([contact, ...contacts]);
-          ToastAndroid.show(
-            Translate('Toast.Contact.Added'),
-            ToastAndroid.LONG,
-          );
-        } else throw new Error(Translate('Toast.Contact.FailedToAdd'));
+        DataBase.db.transaction(tx => {
+          tx.executeSql('INSERT INTO contacts(id, name, country_code, phone, country, createdAt) VALUES(?,?,?,?,?,?)', [id, name, country_code, phone, country, createdAt], (_, result) => {
+            if (result.rowsAffected === 1) {
+              setContacts([contact, ...contacts]);
+              ToastAndroid.show(Translate('Toast.Contact.Added'), ToastAndroid.LONG);
+            } else throw new Error(Translate('Toast.Contact.FailedToAdd'));
+          });
+        });
       } catch (err) {
         const error = err as Error;
         ToastAndroid.show(error.message, ToastAndroid.LONG);
       }
     },
-    [dbConnection, contacts, Translate],
+    [contacts, Translate],
   );
 
   const editContact = useCallback(
@@ -65,10 +53,7 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
       try {
         const { id, name, country, country_code, phone } = contact;
 
-        const update = await dbConnection?.executeSql(
-          'UPDATE contacts SET country=?, country_code=?, phone=?, name=? WHERE id = ?',
-          [country, country_code, phone, name, id],
-        );
+        const update = await DataBase.db.executeSql('UPDATE contacts SET country=?, country_code=?, phone=?, name=? WHERE id = ?', [country, country_code, phone, name, id]);
 
         if (update) {
           const updatedList = contacts.map(item => {
@@ -77,10 +62,7 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
           });
 
           setContacts(updatedList);
-          ToastAndroid.show(
-            Translate('Toast.Contact.Updated'),
-            ToastAndroid.LONG,
-          );
+          ToastAndroid.show(Translate('Toast.Contact.Updated'), ToastAndroid.LONG);
           return update[0].rowsAffected > 0;
         } else throw new Error(Translate('Toast.Contact.FailedToUpdate'));
       } catch (err) {
@@ -89,7 +71,7 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
         return false;
       }
     },
-    [dbConnection, contacts, Translate],
+    [contacts, Translate],
   );
 
   const removeContact = useCallback(
@@ -110,19 +92,13 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
       if (!confirm) return false;
 
       try {
-        const deleteResult = await dbConnection?.executeSql(
-          'DELETE FROM contacts WHERE id = ?',
-          [contact.id],
-        );
+        const deleteResult = await DataBase.db.executeSql('DELETE FROM contacts WHERE id = ?', [contact.id]);
 
         if (deleteResult) {
           const filtered = contacts.filter(item => item.id !== contact.id);
 
           setContacts(filtered);
-          ToastAndroid.show(
-            Translate('Toast.Contact.Deleted'),
-            ToastAndroid.LONG,
-          );
+          ToastAndroid.show(Translate('Toast.Contact.Deleted'), ToastAndroid.LONG);
           return deleteResult[0].rowsAffected > 0;
         } else throw new Error('Toast.Contact.FailedToDelete');
       } catch (err) {
@@ -132,7 +108,7 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
 
       return false;
     },
-    [dbConnection, contacts, Translate],
+    [contacts, Translate],
   );
 
   const clearContacts = useCallback(async () => {
@@ -155,20 +131,17 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
 
       if (!confirm) return false;
 
-      const result = await dbConnection?.executeSql('DELETE FROM contacts');
+      const result = await DataBase.db.executeSql('DELETE FROM contacts');
 
       if (result) {
         setContacts([]);
-        ToastAndroid.show(
-          Translate('Toast.Contact.HistoryCleared'),
-          ToastAndroid.LONG,
-        );
+        ToastAndroid.show(Translate('Toast.Contact.HistoryCleared'), ToastAndroid.LONG);
       } else throw new Error(Translate('Toast.Contact.FailedToClearAll'));
     } catch (err) {
       const error = err as Error;
       ToastAndroid.show(error.message, ToastAndroid.LONG);
     }
-  }, [dbConnection, Translate]);
+  }, [Translate]);
 
   const openWhatsApp = useCallback(
     async (phone: string) => {
@@ -178,16 +151,10 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
       const supported = await Linking.canOpenURL(whatsappURL);
 
       if (supported) {
-        ToastAndroid.show(
-          Translate('Toast.OpeningWhatsApp'),
-          ToastAndroid.LONG,
-        );
+        ToastAndroid.show(Translate('Toast.OpeningWhatsApp'), ToastAndroid.LONG);
         Linking.openURL(whatsappURL);
       } else {
-        Alert.alert(
-          Translate('Alerts.Error'),
-          Translate('Alerts.WhatsAppCantHandleURL'),
-        );
+        Alert.alert(Translate('Alerts.Error'), Translate('Alerts.WhatsAppCantHandleURL'));
       }
     },
     [Translate],
@@ -195,17 +162,17 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
 
   useEffect(() => {
     async function loadContacts(): Promise<void> {
-      const result = await dbConnection?.executeSql('SELECT * FROM contacts');
-
-      if (result !== undefined) {
-        setContacts(result[0].rows.raw().reverse());
-        setLoading(false);
-      }
+      DataBase.db.transaction(tx => {
+        tx.executeSql('SELECT * FROM contacts', [], (_, result) => {
+          setContacts(result.rows.raw().reverse());
+          setLoading(false);
+        });
+      });
     }
 
     setLoading(true);
     loadContacts();
-  }, [dbConnection]);
+  }, []);
 
   const value = useMemo(
     () => ({
@@ -217,20 +184,10 @@ const ContactProvider: React.FC<ContactProviderProps> = ({ children }) => {
       openWhatsApp,
       clearContacts,
     }),
-    [
-      addContact,
-      editContact,
-      removeContact,
-      contacts,
-      loading,
-      openWhatsApp,
-      clearContacts,
-    ],
+    [addContact, editContact, removeContact, contacts, loading, openWhatsApp, clearContacts],
   );
 
-  return (
-    <ContactContext.Provider value={value}>{children}</ContactContext.Provider>
-  );
+  return <ContactContext.Provider value={value}>{children}</ContactContext.Provider>;
 };
 
 function useContact(): ContactContext {
