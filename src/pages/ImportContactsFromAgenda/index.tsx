1b4f38c62b4351077dbd8dd9a7573b604135570f
fellypsantos <fellypsantos2011@gmail.com>
2024-09-28 10:41:00 -0300

    wip: add buttons to footer

--- src/pages/ImportContactsFromAgenda/index.tsx
+++ src/pages/ImportContactsFromAgenda/index.tsx
@@ -1,14 +1,22 @@
 import React, { useCallback, useState } from 'react';
-import { Alert, Button, Linking, Text } from 'react-native';
+import { Alert, Linking } from 'react-native';
 import Contacts from 'react-native-contacts';
 import { PermissionsAndroid } from 'react-native';
-import { Contact } from 'react-native-contacts/type';
 import { FlatList } from 'react-native-gesture-handler';
 import CheckBox from '@react-native-community/checkbox';
-import { SelectableContactDisplayName, SelectableContactToImport, SelectableContactToImportView } from './styles';
+import { Container, BottomButtonContainer, SelectableContactDisplayName, SelectableContactToImport, SelectableContactToImportView, ToggleSelectAllContacts } from './styles';
+import ButtonComponent from '../../components/Button';
+import { Contact } from 'react-native-contacts/type';
+import { convertContactToContactImportItem } from './services/contactImportService';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+export type ContactImportItemType = Contact & {
+  selected: boolean;
+}
 
 export default function ImportContactsFromAgenda() {
-  const [contactsFromAgenda, setContactsFromAgenda] = useState<Contact[]>([]);
+  const [allContactsChecked, setAllContactsChecked] = useState(false);
+  const [contactsFromAgenda, setContactsFromAgenda] = useState<ContactImportItemType[]>([]);
 
   const manuallyAllowReadContactsMessage = useCallback(() => {
     Alert.alert('Permission Denied', 'You need to allow contact access from the settings.', [
@@ -31,7 +39,10 @@ export default function ImportContactsFromAgenda() {
 
         if (res === 'granted') {
           Contacts.getAll()
-            .then(setContactsFromAgenda)
+            .then((contacts: Contact[]) => {
+              const contactsImportList = convertContactToContactImportItem(contacts);
+              setContactsFromAgenda(contactsImportList);
+            })
             .catch(e => {
               console.log(e);
             });
@@ -42,27 +53,60 @@ export default function ImportContactsFromAgenda() {
       });
   }, [manuallyAllowReadContactsMessage]);
 
+  const handleToggleCheckContact = useCallback((contactToToggleChecState: ContactImportItemType) => {
+    const updatedContactList = contactsFromAgenda.map(contact => {
+      if (contact.recordID === contactToToggleChecState.recordID) { return { ...contact, selected: !contact.selected }; }
+      return contact;
+    });
+
+    setContactsFromAgenda(updatedContactList);
+  }, [contactsFromAgenda]);
+
+  const handleToggleCheckAllContacts = useCallback(() => {
+    const contactListToggledState = contactsFromAgenda.map(contact => ({ ...contact, selected: !allContactsChecked }));
+    setAllContactsChecked(!allContactsChecked);
+    setContactsFromAgenda(contactListToggledState);
+  }, [allContactsChecked, contactsFromAgenda]);
+
   const renderContactListFromAgenda = useCallback(
-    ({ item }: { item: Contact }) => (
+    ({ item }: { item: ContactImportItemType }) => (
       <SelectableContactToImport
         key={item.recordID}
         onPress={() => {
-          // todo
+          handleToggleCheckContact(item);
         }}>
         <SelectableContactToImportView>
-          <CheckBox disabled={false} value={true} onValueChange={() => { }} tintColors={{ true: '#5467FB' }} />
+          <CheckBox disabled={false} value={item.selected} onValueChange={() => { }} tintColors={{ true: '#5467FB' }} />
           <SelectableContactDisplayName>{item.displayName}</SelectableContactDisplayName>
         </SelectableContactToImportView>
       </SelectableContactToImport>
     ),
-    [],
+    [handleToggleCheckContact],
   );
 
   return (
-    <React.Fragment>
-      <Button onPress={handleReadContactsFromAgenda} title="sample" />
 
-      <FlatList data={contactsFromAgenda} keyExtractor={contact => contact.recordID} renderItem={renderContactListFromAgenda} />
+    <React.Fragment>
+      {contactsFromAgenda.length === 0 && (
+        <Container>
+          <ButtonComponent text="Importar da Agenda" type="default" onPress={handleReadContactsFromAgenda} />
+          <ButtonComponent text="Voltar" type="cancel" onPress={handleReadContactsFromAgenda} />
+        </Container>
+      )}
+
+      {contactsFromAgenda.length > 0 && (
+        <React.Fragment>
+          <FlatList data={contactsFromAgenda} keyExtractor={contact => contact.recordID} renderItem={renderContactListFromAgenda} />
+
+          <BottomButtonContainer>
+            <ToggleSelectAllContacts onPress={handleToggleCheckAllContacts}>
+              <Icon name={allContactsChecked ? 'square' : 'check-square'} color="#fff" size={18} />
+            </ToggleSelectAllContacts>
+            <ButtonComponent text="Importar Selecionados" type="default" onPress={handleReadContactsFromAgenda} fillWidth disabled />
+          </BottomButtonContainer>
+        </React.Fragment>
+      )}
     </React.Fragment>
+
   );
 }
--- src/pages/ImportContactsFromAgenda/styles.ts
+++ src/pages/ImportContactsFromAgenda/styles.ts
@@ -1,6 +1,13 @@
 import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
+export const Container = styled.View`
+  flex: 1;
+  margin: 0 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const SelectableContactToImport = styled(RectButton)``;
 
 export const SelectableContactToImportView = styled.View`
@@ -15,4 +22,20 @@ export const SelectableContactDisplayName = styled.Text`
   font-family: 'Ubuntu-R';
   font-size: 16px;
   color: #474747;
+`;
+
+export const BottomButtonContainer = styled.View`
+  padding: 10px 10px 0px 10px;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const ToggleSelectAllContacts = styled(RectButton)`
+  background-color: #5467fb;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 10px 15px;
+  margin-right: 10px;
+  align-items: center;
+  justify-content: center;
 `;
