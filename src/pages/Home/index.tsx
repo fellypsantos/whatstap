e8e7883c94f1386b93f0d1b802fed1eae9facce8
fellypsantos <fellypsantos2011@gmail.com>
2024-09-29 14:32:51 -0300

    chore: always sort contacts before display

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -95,7 +95,20 @@ const Home: React.FC = () => {
   );
 
   const contactListToRender = useMemo(() => {
-    return contacts.filter(handleFilterSearchContacts);
+    return contacts
+      .sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      })
+      .filter(handleFilterSearchContacts);
   }, [contacts, handleFilterSearchContacts]);
 
   return (
