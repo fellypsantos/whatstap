d1b6863497b0180fd64af8bffac7dd6aec7c6fdb
fellypsantos <fellypsantos2011@gmail.com>
2024-09-29 13:12:58 -0300

    chore: function to create delays in code

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,1 +1,3 @@
 export const isNumeric = (contentToCheck: string) => !isNaN(Number(contentToCheck));
+
+export const sleep = (milliseconds: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, milliseconds));
