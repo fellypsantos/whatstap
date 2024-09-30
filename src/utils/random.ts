165f913d0a5608e1480d3ec4cc81b41b4cb11e1e
fellypsantos <fellypsantos2011@gmail.com>
2024-09-30 20:34:00 -0300

    chore: add binary chance for other ad options

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,4 +1,6 @@
-export const CalculateChanceToDisplayAppOpenAd = () => {
+export const CalculateChanceToDisplayAppOpenAd = (useBinaryChance: boolean = false) => {
+    if (useBinaryChance) { return Boolean(Math.floor(Math.random() * 2)); }
+
     const randomValue = Math.random() * 100; // Generate a number between 0 and 100
     return randomValue >= 60; // 40% chance to return true
 };
